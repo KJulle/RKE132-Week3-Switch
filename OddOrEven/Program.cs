4f36fddff87345cc7571232a089fe09b55975184
Kätriin Julle <katriinjulle@gmail.com>
2022-10-05 14:39:56 +0300

    week 3 switch

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+//kontrollitakse kas kasutaja sisestatud arv on paaris või paaritu
+
+int result = userNum % 2;
+
+if (result != 0)
+{
+    Console.WriteLine("This number is odd.");
+}
+else
+{
+    Console.WriteLine("This number is even.");
+}
